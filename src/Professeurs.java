481e990d99cf8d26f98f0f47149ad2cc11c9f507
AnthaLeO <cecile.pepy@gmail.com>
2017-10-09 14:14:31 +0200

    class prof

--- src/Professeurs.java
+++ src/Professeurs.java
@@ -1,4 +1,36 @@
 
 public class Professeurs {
+	
+	private String nom, prenom, matiere;
+	
+	public Professeurs(String nom, String prenom, String matiere){
+		this.nom=nom;
+		this.prenom=prenom;
+		this.matiere=matiere;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getMatiere() {
+		return matiere;
+	}
+
+	public void setMatiere(String matiere) {
+		this.matiere = matiere;
+	}
 
 }
